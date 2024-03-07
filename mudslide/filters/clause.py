6ec09afb7dc2df179c4a707a138b466313e10324
Yuanqing Wang <wangyq+gh@wangyq.net>
2024-03-07 00:31:55 -0500

    AndClause

--- mudslide/filters/clause.py
+++ mudslide/filters/clause.py
@@ -1,7 +1,21 @@
+import abc
 from .property import Property
 from .relation import Relation
+from typing import Union
 
-class Clause(object):
+class Clause(abc.ABC):
+    """Base class for all clauses. """
+    @abc.abstractmethod
+    def __call__(self, mol) -> bool:
+        """Filter a molecule. """
+        raise NotImplementedError
+    
+    def __str__(self) -> str:
+        raise NotImplementedError
+    
+    __repr__ = __str__
+
+class SingleClause(Clause):
     """A clause describing the properties of a molecule.
 
 
@@ -10,12 +24,24 @@ class Clause(object):
             self,
             property: Property,
             relation: Relation,
+            value: Union[int, float],
     ):
         self.property = property
         self.relation = relation
+        self.value = value
+
+    def __call__(self, mol) -> bool:
+        return self.relation(self.property(mol), self.value)
 
     def __str__(self) -> str:
         return f"{self.property} {self.relation}"
     
-    __repr__ = __str__
+    __repr__ = __str__ 
+
+class AndClause(Clause):
+    """A clause describing the properties of a molecule.
+
     
+    """
+
+
