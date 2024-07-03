da8ccba3764e0190675b0a6c01a745b6c2a2c7be
Yuanqing Wang <wangyq@wangyq.net>
2024-07-03 10:25:56 +0800

    bonded force

--- mudslide/forces/periodic_torsion_force.py
+++ mudslide/forces/periodic_torsion_force.py
@@ -39,7 +39,7 @@ class PeriodicTorsionForce(NamedTuple):
     @classmethod
     def from_openmm(cls, force):
         """Initialize the force from an OpenMM PeriodicTorsionForce object."""
-        particle1, particle2, particle3, particle4, phase, k, periodicity = (
+        particle1, particle2, particle3, particle4, periodicity, phase, k = (
             [],
             [],
             [],
@@ -54,9 +54,9 @@ class PeriodicTorsionForce(NamedTuple):
                 _particle2,
                 _particle3,
                 _particle4,
+                _periodicity,
                 _phase,
                 _k,
-                _periodicity,
             ) = force.getTorsionParameters(idx)
             particle1.append(_particle1)
             particle2.append(_particle2)
@@ -70,6 +70,64 @@ class PeriodicTorsionForce(NamedTuple):
             particle2=jnp.array(particle2, dtype=jnp.int32),
             particle3=jnp.array(particle3, dtype=jnp.int32),
             particle4=jnp.array(particle4, dtype=jnp.int32),
+            periodicity=jnp.array(periodicity, dtype=jnp.int32),
             phase=jnp.array(phase, dtype=jnp.float64),
             k=jnp.array(k, dtype=jnp.float64),
         )
+
+    def to_openmm(self):
+        """Convert the force to an OpenMM PeriodicTorsionForce object."""
+        from openmm import PeriodicTorsionForce as _PeriodicTorsionForce
+
+        force = _PeriodicTorsionForce()
+        for (
+            particle1,
+            particle2,
+            particle3,
+            particle4,
+            periodicity,
+            phase,
+            k,
+        ) in zip(
+            self.particle1,
+            self.particle2,
+            self.particle3,
+            self.particle4,
+            self.periodicity,
+            self.phase,
+            self.k,
+        ):
+            force.addTorsion(
+                int(particle1),
+                int(particle2),
+                int(particle3),
+                int(particle4),
+                int(periodicity),
+                float(phase),
+                float(k),
+            )
+        return force
+
+    def energy(
+        self,
+        X: jnp.ndarray,
+    ):
+        """Compute the energy."""
+        # Compute the vectors between the particles.
+        r12 = X[self.particle2] - X[self.particle1]
+        r23 = X[self.particle3] - X[self.particle2]
+        r34 = X[self.particle4] - X[self.particle3]
+
+        # Compute the normal vectors.
+        left = jnp.cross(r12, r23)
+        right = jnp.cross(r23, r34)
+
+        # Compute the torsion angle.
+        phi = jnp.atan2(
+            jnp.linalg.norm(jnp.cross(left, right), axis=-1),
+            jnp.sum(left * right, axis=-1),
+        )
+
+        # Compute the energy.
+        energy = self.k * (1 + jnp.cos(self.periodicity * phi - self.phase))
+        return energy
--- mudslide/forces/tests/test_construction.py
+++ mudslide/forces/tests/test_construction.py
@@ -15,3 +15,12 @@ def test_harmonic_angle_force():
     force = HarmonicAngleForce.from_openmm(_force)
     __force = force.to_openmm()
     assert _force.getNumAngles() == __force.getNumAngles()
+
+def test_periodic_torsion_force():
+    import openmm
+    _force = openmm.PeriodicTorsionForce()
+    _force.addTorsion(0, 1, 2, 3, 1, 1.0, 1.0)
+    from mudslide.forces.periodic_torsion_force import PeriodicTorsionForce
+    force = PeriodicTorsionForce.from_openmm(_force)
+    __force = force.to_openmm()
+    assert _force.getNumTorsions() == __force.getNumTorsions()
